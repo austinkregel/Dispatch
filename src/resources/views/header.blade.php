ab59e5c014d489656aede4d6fbcead00be109cc7
Austin Kregel <austinkregel@gmail.com>
2016-02-29 15:06:50 -0500

    Add jumbotron coloring

--- src/resources/views/header.blade.php
+++ src/resources/views/header.blade.php
@@ -1,5 +1,5 @@
 <div class="col-md-12" style="padding:0;margin-top:-2rem;">
-    <div class="jumbotron">
+    <div class="jumbotron" style="background:#{{ config('kregel.dispatch.color') }}">
         <div class="container">
             <h2>{{ $header }}</h2>
         </div>

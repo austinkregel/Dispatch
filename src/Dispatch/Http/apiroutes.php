656e5d96db5608cfb8d6b6088a8e1eae9f769935
Austin Kregel <austinkregel@gmail.com>
2016-02-29 14:59:28 -0500

    Remove api routes,because they're not used

--- src/Dispatch/Http/apiroutes.php
+++ src/Dispatch/Http/apiroutes.php
@@ -1,48 +1,48 @@
 <?php
 
 Route::group(['prefix' => 'api/v1.0'], function () {
-
-/*
- * This is the group for uploading
- * @return void
- */
-Route::group(['prefix' => 'upload'], function () {
-    Route::post('document', ['as' => 'upload.document', 'uses' => 'Api\Upload\Upload@storeDocument']);
-    Route::post('image', ['as' => 'upload.document', 'uses' => 'Api\Upload\Upload@storeImage']);
-    Route::post('video', ['as' => 'upload.document', 'uses' => 'Api\Upload\Upload@storeVideo']);
-});
-/*
- * This is the group for creating objects
- * @return void
- */
-Route::group(['prefix' => 'create'], function () {
-//            Route::post('document', '');
-//            Route::post('image', '');
-//            Route::post('video', '');
-    Route::post('{model}', 'Api\Create\Create@create');// I'm throwing in ALLLL the creates :3
-});
-
-/*
- * This a group for updating (put requests)
- * @return void
- */
-Route::group(['prefix' => 'update'], function () {
-//            Route::put('document/{uuid}', '');
-//            Route::put('image/{uuid}', '');
-//            Route::put('video/{uuid}', '');
-//            Route::put('{models}/{uuid}', '');
-});
-
-/*
- * This a group for deleting (delete requests)
- * @return void
- */
-Route::group(['prefix' => 'delete.'], function () {
-//            Route::delete('document/{uuid}', '');
-//            Route::delete('image/{uuid}', '');
-//            Route::delete('video/{uuid}', '');
-//            Route::delete('{models}/{uuid}', '');
-});
+//
+///*
+// * This is the group for uploading
+// * @return void
+// */
+//Route::group(['prefix' => 'upload'], function () {
+//    Route::post('document', ['as' => 'upload.document', 'uses' => 'Api\Upload\Upload@storeDocument']);
+//    Route::post('image', ['as' => 'upload.document', 'uses' => 'Api\Upload\Upload@storeImage']);
+//    Route::post('video', ['as' => 'upload.document', 'uses' => 'Api\Upload\Upload@storeVideo']);
+//});
+///*
+// * This is the group for creating objects
+// * @return void
+// */
+//Route::group(['prefix' => 'create'], function () {
+////            Route::post('document', '');
+////            Route::post('image', '');
+////            Route::post('video', '');
+//    Route::post('{model}', 'Api\Create\Create@create');// I'm throwing in ALLLL the creates :3
+//});
+//
+///*
+// * This a group for updating (put requests)
+// * @return void
+// */
+//Route::group(['prefix' => 'update'], function () {
+////            Route::put('document/{uuid}', '');
+////            Route::put('image/{uuid}', '');
+////            Route::put('video/{uuid}', '');
+////            Route::put('{models}/{uuid}', '');
+//});
+//
+///*
+// * This a group for deleting (delete requests)
+// * @return void
+// */
+//Route::group(['prefix' => 'delete.'], function () {
+////            Route::delete('document/{uuid}', '');
+////            Route::delete('image/{uuid}', '');
+////            Route::delete('video/{uuid}', '');
+////            Route::delete('{models}/{uuid}', '');
+//});
 
 /*
  * This is the default retrieval for various models

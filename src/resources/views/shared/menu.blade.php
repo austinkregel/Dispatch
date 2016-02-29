976459a20fbf9408f1f2479f3d02c19f95b6ed74
Austin Kregel <austinkregel@gmail.com>
2016-02-29 15:09:32 -0500

    Add closed tickets menu

--- src/resources/views/shared/menu.blade.php
+++ src/resources/views/shared/menu.blade.php
@@ -1,4 +1,3 @@
-
 <div class="panel panel-default panel-flush">
     <div class="panel-heading @if(config('app.debug')) themer--secondary @endif" style="text-transform:none;">
         Did something happen? File a ticket!
@@ -23,7 +22,7 @@
                                    class="p-link">
                                     <i class="fa fa-btn fa-fw fa-cog"></i>View {{ $jur->name }}
                                     @if(!$jur->tickets->isEmpty())
-                                        <span class="badge text-right" style="color:white;">{{ $jur->tickets->count( )}}</span>
+                                        <span class="badge text-right" style="color:white;">{{ $jur->tickets()->where('deleted_at', NULL)->get()->count( )}}</span>
                                     @endif
                                 </a>
                             </li>
@@ -49,3 +48,42 @@
         </div>
     </div>
 </div>
+@if(!empty($jurisdiction))
+    <div class="panel panel-default panel-flush">
+        <div class="panel-heading @if(config('app.debug')) themer--secondary @endif" style="text-transform:none;">
+            Closed Tickets
+        </div>
+        <div class="spark-panel-body panel-body">
+            <div class="spark-settings-tabs">
+                <ul class="nav-wrapper nav spark-settings-tabs-stacked" role="tablist">
+                    <!-- Authenticated Right Dropdown -->
+                    @if(auth()->user()->can('create-ticket'))  <!-- Settings -->
+                    <li class="dropdown submenu">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                           aria-expanded="false">Tickets <i class="fa fa-chevron-down"></i></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="{{ route('dispatch::new.ticket') }}" class="p-link">
+                                    New Ticket
+                                </a>
+                            </li>
+                            @foreach (auth()->user()->jurisdiction as $jur)
+                                <li>
+                                    <a href="{{ route('dispatch::view.ticket', [str_slug($jur->name)]) }}"
+                                       class="p-link">
+                                        <i class="fa fa-btn fa-fw fa-cog"></i>View {{ $jur->name }}
+                                        @if(!$jur->tickets()->whereRaw('deleted_at is not null')->get()->isEmpty())
+                                            <span class="badge text-right" style="color:white;">{{ $jur->tickets()->whereRaw('deleted_at is not null')->get()->count( )}}</span>
+                                        @endif
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </li>
+                    @endif
+
+                </ul>
+            </div>
+        </div>
+    </div>
+@endif

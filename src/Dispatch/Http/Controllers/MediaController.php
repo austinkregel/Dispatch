8dbdafb10c96a43965af2546112b2068a5816a5d
Austin Kregel <austinkregel@gmail.com>
2016-03-02 18:11:37 -0500

    Add  media controller

--- src/Dispatch/Http/Controllers/MediaController.php
+++ src/Dispatch/Http/Controllers/MediaController.php
@@ -1,15 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sodium-chloride
- * Date: 3/2/2016
- * Time: 4:28 PM
- */
 
 namespace Kregel\Dispatch\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Kregel\Dispatch\Models\Jurisdiction;
+use Kregel\Dispatch\Models\Photos;
+use Kregel\FormModel\FormModel;
 
-class MediaController
+class MediaController extends Controller
 {
+    public function showMedia($uuid){
+        $media = Photos::whereUuid($uuid)->first();
+        if(empty($media) || $uuid === '159a50e6-5382-4a52-ae94-3f5a4e8f0584'){
+            return response()->make(file_get_contents(storage_path('app/media/159a50e6-5382-4a52-ae94-3f5a4e8f0584.jpeg')))->header('Content-type','image/jpeg')->header('Content-length', filesize(storage_path($media->path)));
+        }
+        if($media->type == 'doc')
+            return $this->media('application/pdf', $media);
+        else{
+            $str = substr($media->path, -4);
+            switch(trim($str, '.')){
+                case 'jpg':
+                case 'jpeg':
+                    return $this->media('image/jpeg', $media);
+                    break;
+                case 'png':
+                    return $this->media('image/png', $media);
+                    break;
+                case 'gif':
+                    return $this->media('image/gif', $media);
+                    break;
+            }
+        }
+    }
+    private function media($type, $media){
+        return response()->make(file_get_contents(storage_path($media->path)))->header('Content-type',$type)->header('Content-length', filesize(storage_path($media->path)));
+    }
 
 }

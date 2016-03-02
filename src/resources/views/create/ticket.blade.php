83a78456db052b28f897739a52a1fc7f5902abdc
Austin Kregel <austinkregel@gmail.com>
2016-03-02 18:16:06 -0500

    Update header and various other parts

--- src/resources/views/create/ticket.blade.php
+++ src/resources/views/create/ticket.blade.php
@@ -1,9 +1,10 @@
 @extends(config('kregel.dispatch.view-base'))
+
 @section('content')
     <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
-    @include('dispatch::header', ['header' => 'Showing Jurisdictions'])
+    @include('dispatch::header', ['header' => 'File a ticket'])
 
     <div class="container spark-screen">
         <div class="row">
@@ -12,14 +13,6 @@
             </div>
             <div class="col-md-8">
                 <div class="panel panel-default">
-                    <div class="panel-heading">
-                        @if(!empty($jurisdiction))
-                            File a ticket for {{ $jurisdiction->name }}
-                        @else
-                            File a ticket
-                        @endif
-
-                    </div>
                     <div class="panel-body">
                         @include('dispatch::shared.errors')
                         <div id="vue-form-wrapper">
@@ -33,23 +26,14 @@
                                 <input class="form-control" id="_method" type="hidden" name="_method" value="post">
                             </div>
                             <div class="form-group">
-                                <input class="form-control" id="_redirect" type="hidden" name="_redirect" value="{{ route('dispatch::view.ticket', [str_slug($jurisdiction->name)]) }}">
-                            </div>
-                            <div class="form-group">
                                 <input class="form-control" v-model="data.owner_id" id="owner_id" type="hidden"
                                        name="owner_id" value="{{ auth()->user()->id }}">
-                            </div>
-                            <div class="form-group">
-                                <input class="form-control" v-model="data.jurisdiction_id" id="jurisdiction_id"
-                                       type="hidden" name="jurisdiction_id" value="{{ $jurisdiction->id }}">
                             </div>
                             {!! csrf_field() !!}
 
                             <div class="form-group">
                                 <label for="title">Title</label>
-
-                                <textarea class="form-control" cols="3" id="title" type="text" name="title"
-                                          v-model="data.title"></textarea>
+                                <input class="form-control" id="title" type="text" name="title" v-model="data.title">
                             </div>
                             <div class="form-group">
                                 <label for="body">Body</label>
@@ -58,12 +42,7 @@
                                           v-model="data.body"></textarea>
                             </div>
                             <div class="form-group">
-                                <label for="finish_by">Finish by (optional)</label>
-
-                                <input class="form-control" id="finish_by" type="datetime" name="finish_by"
-                                          v-model="data.finish_by">
-                            </div>
-                            <div class="form-group">
+                                <label>Ticket priority</label>
                                 <select id="priority_id" default="" type="select" name="priority_id"
                                         v-model="data.priority_id" class="form-control">
                                     <option value="" disabled selected>Please select a priority to assign this to
@@ -74,39 +53,34 @@
                                 </select>
 
                             </div>
-                            @if(empty($jurisdiction))
+                            <div class="form-group">
+                                <label for="finish_by">Finish by (optional)</label>
+
+                                <input class="form-control" id="finish_by" type="datetime" name="finish_by"
+                                       v-model="data.finish_by">
+                            </div>
+                            @if(!empty($jurisdiction))
                                 <div class="form-group">
-                                    <select id="jurisdiction" v-model="data.jurisdiction_id">
+                                    <label for="jurisdiction_id">Jurisdiction it belongs to?</label>
+                                    <select id="jurisdiction" v-model="data.jurisdiction_id" class="form-control"
+                                            name="jurisdiction_id">
                                         <option value="" disabled selected>Please assign this to a location</option>
-                                        @foreach(auth()->user()->jurisdiction as $jurisdiction)
-                                            <option value="{{ $jurisdiction->id }}">{{ $jurisdiction->name }}</option>
+                                        @foreach($jurisdiction as $j)
+                                            <option value="{{ $j->id }}">{{ $j->name }}</option>
                                         @endforeach
 
                                     </select>
                                 </div>
-                            @endif
-                            @if(auth()->user()->can_assign())
-                                <div class="form-group">
-                                    <select multiple id="assign_to" class="form-control" v-model="data.assign_to">
-                                        <option value="" disabled selected>Please assign a user or two</option>
-                                        @foreach($jurisdiction->users as $user)
-                                            <option value="{{ $user->id }}">{{ $user->name }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-
-
                             @endif
                             <div class="form-group">
                                 <div class="form-group">
                                     <input class="btn btn-primary pull-right" id="" type="submit">
                                 </div>
                             </div>
-
                         </form>
                     </div>
-
                 </div>
             </div>
         </div>
-    </div>@endsection
+    </div>
+@endsection
